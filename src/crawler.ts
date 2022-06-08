229873459689c7c9eba6d8e517ea7ee8453645b7
jrhe1213 <jiarong1213@gmail.com>
2022-06-07 23:18:22 -0400

    add cheerio

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,13 +1,41 @@
 import superagent from "superagent";
+import cheerio from "cheerio";
+
+interface Course {
+  title: string;
+  count: number;
+}
 
 class Crawler {
   private secret = "this is my super secret";
   private url = "https://laminasolutions.com/services";
-  private rawHtml = "";
+
+  getServiceInfo(rawHtml: string) {
+    const courseInfos: Course[] = [];
+
+    const $ = cheerio.load(rawHtml);
+    const serviceItems = $(".et_pb_text_inner");
+
+    serviceItems.map((_, element) => {
+      const service = $(element).find("li");
+      const title = service.eq(0).text();
+      const count = Math.floor(Math.random() * 50);
+      courseInfos.push({
+        title,
+        count,
+      });
+    });
+
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos,
+    };
+    console.log("result: ", result);
+  }
 
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    this.rawHtml = result.text;
+    this.getServiceInfo(result.text);
   }
 
   constructor() {
