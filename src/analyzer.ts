14e0caf22795df084aef2c9df137359fbbcc93e0
jrhe1213 <jiarong1213@gmail.com>
2022-06-08 20:50:09 -0400

    split analyzer from crawler class & refactor

---
+++ src/analyzer.ts
@@ -0,0 +1,59 @@
+import fs from "fs";
+import cheerio from "cheerio";
+
+import { IAnalyzer } from "./crawler";
+
+export interface Course {
+  title: string;
+  count: number;
+}
+
+export interface CourseResult {
+  time: number;
+  data: Course[];
+}
+
+export interface Content {
+  [propName: number]: Course[];
+}
+
+export default class Analyzer implements IAnalyzer {
+  private getTargetInfo(html: string) {
+    const courses: Course[] = [];
+    // cheerio jquery search
+    const $ = cheerio.load(html);
+    // get target items
+    const courseItems = $(".et_pb_text_inner");
+    courseItems.map((_, element) => {
+      const course = $(element).find("li");
+      const title = course.eq(0).text();
+      const count = Math.floor(Math.random() * 50);
+      if (title !== "") {
+        courses.push({
+          title,
+          count,
+        });
+      }
+    });
+    return {
+      time: new Date().getTime(),
+      data: courses,
+    };
+  }
+
+  private generateJsonContent(courses: CourseResult, filePath: string) {
+    let fileContent: Content = {};
+    // load existing content
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    }
+    fileContent[courses.time] = courses.data;
+    return fileContent;
+  }
+
+  public analyze(html: string, filePath: string) {
+    const items = this.getTargetInfo(html);
+    const fileContent = this.generateJsonContent(items, filePath);
+    return JSON.stringify(fileContent);
+  }
+}
--- src/crawler.ts
+++ src/crawler.ts
@@ -1,80 +1,43 @@
 import fs from "fs";
 import path from "path";
 import superagent from "superagent";
-import cheerio from "cheerio";
+import Analyzer from "./analyzer";
 
-interface Course {
-  title: string;
-  count: number;
-}
-
-interface CourseResult {
-  time: number;
-  data: Course[];
-}
-
-interface Content {
-  [propName: number]: Course[];
+export interface IAnalyzer {
+  analyze: (html: string, filePath: string) => string;
 }
 
 class Crawler {
-  private secret = "this is my super secret";
-  private url = `https://laminasolutions.com/services?secure=${this.secret}`;
-  private filePath = path.resolve(__dirname, "../data/course.json");
-
-  getServiceInfo(rawHtml: string) {
-    const courseInfos: Course[] = [];
-    // cheerio
-    const $ = cheerio.load(rawHtml);
-    // get target items
-    const serviceItems = $(".et_pb_text_inner");
-    serviceItems.map((_, element) => {
-      const service = $(element).find("li");
-      const title = service.eq(0).text();
-      const count = Math.floor(Math.random() * 50);
-      if (title !== "") {
-        courseInfos.push({
-          title,
-          count,
-        });
-      }
-    });
-    return {
-      time: new Date().getTime(),
-      data: courseInfos,
-    };
-  }
-
-  async getRawHtml() {
+  private async getRawHtml() {
     const result = await superagent.get(this.url);
     return result.text;
   }
 
-  generateJsonContent(courses: CourseResult) {
-    let fileContent: Content = {};
-    // load existing content
-    if (fs.existsSync(this.filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(this.filePath, "utf-8"));
-    }
-    fileContent[courses.time] = courses.data;
-    return fileContent;
+  private writeFile(content: string) {
+    fs.writeFileSync(this.filePath, content);
   }
 
-  writeFile(fileContent: Content) {
-    // save back
-    fs.writeFileSync(this.filePath, JSON.stringify(fileContent));
-  }
-
-  async initSpiderProcess() {
+  private async initSpiderProcess() {
     const html = await this.getRawHtml();
-    const services = this.getServiceInfo(html);
-    const fileContent = this.generateJsonContent(services);
+    const fileContent = this.analyzer.analyze(html, this.filePath);
     this.writeFile(fileContent);
   }
 
-  constructor() {
+  constructor(
+    private url: string,
+    private analyzer: IAnalyzer,
+    private filePath: string
+  ) {
     this.initSpiderProcess();
   }
 }
 
-const crawler = new Crawler();
+//
+const secret = "this is my super secret";
+const url = `https://laminasolutions.com/services?secure=${secret}`;
+const analyzer = new Analyzer();
+const crawler = new Crawler(
+  url,
+  analyzer,
+  path.resolve(__dirname, "../data/course.json")
+);
