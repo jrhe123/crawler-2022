8aadc9b217eb97cb0fcff86b5c549603f750e3a5
jrhe1213 <jiarong1213@gmail.com>
2022-06-09 22:58:04 -0400

    add bodyparser

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import bodyParser from "body-parser";
 import router from "./router";
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 // routes
 app.use(router);
 
