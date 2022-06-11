e8975120ea39e76583154f09c23d27603d9bff06
jrhe1213 <jiarong1213@gmail.com>
2022-06-11 15:03:11 -0400

    add put/del route

--- src/controller/decorator.ts
+++ src/controller/decorator.ts
@@ -4,6 +4,8 @@ export const router = Router();
 enum Mehod {
   get = "get",
   post = "post",
+  put = "put",
+  delete = "delete",
 }
 
 export function controller(target: any) {
@@ -22,16 +24,16 @@ export function controller(target: any) {
   }
 }
 
-export function get(path: string) {
-  return function (target: any, key: string) {
-    Reflect.defineMetadata("path", path, target, key);
-    Reflect.defineMetadata("method", "get", target, key);
+function getRequestDecorator(type: string) {
+  return function (path: string) {
+    return function (target: any, key: string) {
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", type, target, key);
+    };
   };
 }
 
-export function post(path: string) {
-  return function (target: any, key: string) {
-    Reflect.defineMetadata("path", path, target, key);
-    Reflect.defineMetadata("method", "post", target, key);
-  };
-}
+export const get = getRequestDecorator("get");
+export const post = getRequestDecorator("post");
+export const put = getRequestDecorator("put");
+export const del = getRequestDecorator("delete");
